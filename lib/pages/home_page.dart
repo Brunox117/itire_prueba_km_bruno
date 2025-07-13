02d7f425ef087ca351d5ac1de9aeff343f7f290f
Bruno Jimenez <omarjm0910@gmail.com>
2025-07-13 10:48:31 -0600

    Se hace la llamada a la api desde el provider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,80 +1,17 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:itire_prueba_km_bruno/model/get_messages_api_model.dart';
-import 'package:itire_prueba_km_bruno/model/get_trips_api_model.dart';
-import 'package:itire_prueba_km_bruno/model/login_api_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:itire_prueba_km_bruno/providers/mileage_provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('HomePage')),
       body: OutlinedButton(
         onPressed: () async {
-          try {
-            final dio = Dio();
-            String token = dotenv.env['API_TOKEN'] ?? '';
-            int vehicleID = 734455;
-            final loginResponse = await dio.get(
-              "https://hst-api.wialon.com/wialon/ajax.html?svc=token/login&params={\"token\":\"$token\",\"operateAs\":\"SdkDemo\",\"fl\":8}",
-            );
-            LoginApiModel loginInfo = LoginApiModel.fromMap(loginResponse.data);
-            String sessionId = loginInfo.eid;
-            final headers = {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            };
-            final paramsForMessages = {
-              "itemId": vehicleID,
-              "timeFrom": 0,
-              "timeTo": (DateTime.now().millisecondsSinceEpoch / 1000).round(),
-              "flags": 0,
-              "flagsMask": 65280,
-              "loadCount": 1,
-            };
-            final getMessages = await dio.post(
-              "https://hst-api.wialon.com/wialon/ajax.html?svc=messages/load_interval&sid=$sessionId&params=${Uri.encodeComponent(json.encode(paramsForMessages))}",
-              options: Options(headers: headers),
-            );
-            if (getMessages.data != null) {
-              GetMessagesApiModel messagesData = GetMessagesApiModel.fromMap(
-                getMessages.data,
-              );
-              print("Se cargaron los mensajes $messagesData");
-            }
-            final paramsForTrips = {
-              "itemId": vehicleID,
-              "timeFrom": 1700514427,
-              "timeTo": (DateTime.now().millisecondsSinceEpoch / 1000).round(),
-              "msgsSource": 1,
-            };
-            final getTrips = await dio.post(
-              "https://hst-api.wialon.com/wialon/ajax.html?svc=unit/get_trips&sid=$sessionId&params=${Uri.encodeComponent(json.encode(paramsForTrips))}",
-              options: Options(headers: headers),
-            );
-            if (getTrips.data != null) {
-              GetTripsApiModel tripsData = GetTripsApiModel.fromJson(
-                json.encode(getTrips.data),
-              );
-              if (tripsData.trips.isNotEmpty) {
-                double totalDistance = 0;
-                for (var trip in tripsData.trips) {
-                  totalDistance += trip.distance;
-                }
-                print(
-                  "Total distance segun trips = ${(totalDistance / 1000).toStringAsFixed(2)} km",
-                );
-              } else {
-                print("No se encontraron viajes");
-              }
-            }
-          } catch (error) {
-            debugPrint('Error: $error');
-          }
+          ref.read(mileageProvider.notifier).fetchMileage();
         },
         child: const Text("API CALL"),
       ),
---
+++ lib/providers/mileage_provider.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:itire_prueba_km_bruno/model/get_messages_api_model.dart';
+import 'package:itire_prueba_km_bruno/model/get_trips_api_model.dart';
+import 'package:itire_prueba_km_bruno/model/login_api_model.dart';
+
+final mileageProvider =
+    StateNotifierProvider<MileageNotifier, AsyncValue<double>>(
+      (ref) => MileageNotifier(),
+    );
+
+class MileageNotifier extends StateNotifier<AsyncValue<double>> {
+  final dio = Dio();
+  final int _vehicleId = 734455;
+
+  MileageNotifier() : super(AsyncValue.data(0));
+
+  Future<void> fetchMileage() async {
+    state = const AsyncValue.loading();
+    try {
+      final sessionId = await getSessionId(dio);
+      print("sessionId = $sessionId");
+      int timeFrom = await loadMessages(
+        dio: dio,
+        sessionId: sessionId,
+        vehicleId: _vehicleId,
+      );
+      print("timeFrom = $timeFrom");
+      final totalMileage = await getTotalMileage(
+        dio: dio,
+        sessionId: sessionId,
+        vehicleId: _vehicleId,
+        timeFrom: timeFrom,
+      );
+      print("totalMileage = $totalMileage");
+    } catch (e) {
+      state = AsyncValue.error(e, StackTrace.current);
+    }
+  }
+}
+
+//FUNCTIONS TO CALL THE API
+Future<String> getSessionId(Dio dio) async {
+  String token = dotenv.env['API_TOKEN'] ?? '';
+  final loginResponse = await dio.get(
+    "https://hst-api.wialon.com/wialon/ajax.html?svc=token/login&params={\"token\":\"$token\",\"operateAs\":\"SdkDemo\",\"fl\":8}",
+  );
+  LoginApiModel loginInfo = LoginApiModel.fromMap(loginResponse.data);
+  return loginInfo.eid;
+}
+
+Future<int> loadMessages({
+  required Dio dio,
+  required String sessionId,
+  required int vehicleId,
+}) async {
+  final headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+  final params = {
+    "itemId": vehicleId,
+    "timeFrom": 0,
+    "timeTo": (DateTime.now().millisecondsSinceEpoch / 1000).round(),
+    "flags": 0,
+    "flagsMask": 65280,
+    "loadCount": 1,
+  };
+
+  final messagesResponse = await dio.post(
+    "https://hst-api.wialon.com/wialon/ajax.html?svc=messages/load_interval&sid=$sessionId&params=${Uri.encodeComponent(json.encode(params))}",
+    options: Options(headers: headers),
+  );
+
+  final messagesModel = GetMessagesApiModel.fromMap(messagesResponse.data);
+  return messagesModel.messages[0].timestamp;
+}
+
+Future<double> getTotalMileage({
+  required Dio dio,
+  required String sessionId,
+  required int vehicleId,
+  required int timeFrom,
+}) async {
+  final headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+
+  final params = {
+    "itemId": vehicleId,
+    "timeFrom": timeFrom,
+    "timeTo": (DateTime.now().millisecondsSinceEpoch / 1000).round(),
+    "msgsSource": 1,
+  };
+
+  final getTripsResponse = await dio.post(
+    "https://hst-api.wialon.com/wialon/ajax.html?svc=unit/get_trips&sid=$sessionId&params=${Uri.encodeComponent(json.encode(params))}",
+    options: Options(headers: headers),
+  );
+
+  final tripsData = GetTripsApiModel.fromList(getTripsResponse.data);
+
+  double totalDistance = 0;
+  for (var trip in tripsData.trips) {
+    totalDistance += trip.distance;
+  }
+
+  return totalDistance / 1000;
+}
